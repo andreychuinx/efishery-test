e12ff0ab81e5acdc6a5793a660a563947b43dddf
Andrey Amrulloh <andrey.amrulloh@koinworks.com>
2023-05-01 01:23:51 +0700

    bugfix(aggregate): update checkAuth for role

--- fetch/app.py
+++ fetch/app.py
@@ -12,9 +12,9 @@ app.secret_key = 'TEST'
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 @app.route('/api/price', methods= ['GET'])
-@cache.cached(timeout=600)
+@cache.cached(timeout=600, unless=lambda: request.headers.get('If-Modified-Since'))
 def price():
-    check = checkAuth()
+    check = checkAuth('all')
     if 'error' in check:
        return jsonify({'Error': 'Unauthorized'}), 401
     return getData()
@@ -22,12 +22,14 @@ def price():
 # Create some test data for our catalog in the form of a list of dictionaries.
 
 @app.route('/api/aggregate', methods= ['GET'])
-@cache.cached(timeout=600)
-def getAggregate():
-    check = checkAuth()
+def getAggregateAuth():
+    check = checkAuth('admin')
     if 'error' in check:
-       return jsonify({'Error': 'Unauthorized'}), 401
-    return aggregate()
+        return jsonify({'Error': 'Unauthorized'}), 401
+    @cache.cached(timeout=600)
+    def getAggregate():
+        return aggregate()
+    return getAggregate()
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=3001)
--- fetch/src/middlewares/checkAuth.py
+++ fetch/src/middlewares/checkAuth.py
@@ -1,17 +1,20 @@
 from flask import request, abort, session, jsonify
 from src.config.requestHandler import postRequest
-
-def checkAuth():
+from datetime import datetime
+def checkAuth(role):
     token = request.headers.get('Authorization')
-    if not session.get('user'):
-        response = postRequest('http://localhost:3000/api/check', headers= {
+    response = postRequest('http://localhost:3000/api/check', headers= {
             'Authorization': f'{token}',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            "Cache-Control": "no-cache, no-store, must-revalidate",
+            "Pragma": "no-cache",
+            "Expires": "0",
+            'Cache-Control': 'public, max-age=0',
+            'Last-Modified': datetime.today().strftime('%Y-%m-%d %H:%M:%S')
         })
-        if 'error' in response:
-            return { 'error': 'Authencation failed'}
-        else:
-            session['user'] = response
-            return { 'success': True }
-    else:
-        return { 'success': True}
+    if 'error' in response:
+        return { 'error': 'Authencation failed'}
+    if role != 'all':
+        if role != str(response['role']):
+            return { 'error': 'Authorization failed'}
+    return { 'success': True}
